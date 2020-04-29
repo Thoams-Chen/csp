17c921354def1a72f06910ff4376e1d3a7e30408
Thoams-Chen <47285091+Thoams-Chen@users.noreply.github.com>
2020-04-29 14:52:28 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,40 @@
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    myCorg.x += -5
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    myCorg.y += -5
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    myCorg.x += 5
+})
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    myCorg.y += 5
+})
+let myCorg: Sprite = null
+myCorg = sprites.create(img`
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . f f f f . . . . . . . . . . . . . . . 
+. . . f f f 2 2 f f f . . . . . . . . . . . . . 
+. . f f f 2 2 2 2 f f f . . . . . . . . . . . . 
+. f f f e e e e e e f f f . . . . . . . . . . . 
+. f f e 2 2 2 2 2 2 e e f . . . . . . . . . . . 
+. f e 2 f f f f f f 2 e f . . . . . . . . . . . 
+. f f f f e e e e f f f f . . . . . . . . . . . 
+f f e f . f 4 4 f b f e f f . . . . . . . . . . 
+f e e 4 1 f d d f 1 4 e e f . . . . . . . . . . 
+. f f f f d d d d d e e f . . . . . . . . . . . 
+f d d d d f 4 4 4 e e f . . . . . . . . . . . . 
+f b b b b f 2 2 2 2 f 4 e . . . . . . . . . . . 
+f b b b b f 2 2 2 2 f d 4 . . . . . . . . . . . 
+. f c c f 4 5 5 4 4 f 4 4 . . . . . . . . . . . 
+. . f f f f f f f f . . . . . . . . . . . . . . 
+. . . . f f . . f f . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+scene.setBackgroundImage(scene.backgroundImage())
